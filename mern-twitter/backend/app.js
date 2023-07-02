1e9a33467f6cbf0e157bfc12201e65d12802f269
Ahmed <ahmed96razi39@gmail.com>
2023-07-02 14:48:17 -0700

    setup complete

--- mern-twitter/backend/app.js
+++ mern-twitter/backend/app.js
@@ -1,10 +1,15 @@
+const debug = require('debug');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
 
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
+const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
@@ -13,8 +18,53 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// ADD THIS SECURITY MIDDLEWARE
+// Security Middleware
+if (!isProduction) {
+  // Enable CORS only in development because React will be on the React
+  // development server (http://localhost:3000). (In production, the Express 
+  // server will serve the React files statically.)
+  app.use(cors());
+}
+
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
 
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
+app.use('/api/csrf', csrfRouter);
+
+
+// Express custom middleware for catching all unmatched requests and formatting
+// a 404 error to be sent as the response.
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.statusCode = 404;
+  next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+  serverErrorLogger(err);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    statusCode,
+    errors: err.errors
+  })
+});
 
 module.exports = app;
+
+
